54aed4132faeb42e87b243193ee854d4eb064183
AppleKid7 <kvltd00d@gmail.com>
2016-12-08 16:02:27 -0500

    taking command line argument and displaying in pop up window upon initialization

--- HelloWPF/App.xaml.cs
+++ HelloWPF/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 
 namespace HelloWPF
 {
@@ -16,7 +17,10 @@ namespace HelloWPF
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             MainWindow wnd = new MainWindow();
-            wnd.Title = "Something else";
+            if (e.Args.Length == 1)
+            {
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+            }
             wnd.Show();
         }
     }
