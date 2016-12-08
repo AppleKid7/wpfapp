477a829a982174bf71ac8f779d211498e58d50ac
AppleKid7 <kvltd00d@gmail.com>
2016-12-08 15:40:12 -0500

    working with App.xaml

--- HelloWPF/App.xaml.cs
+++ HelloWPF/App.xaml.cs
@@ -13,5 +13,11 @@ namespace HelloWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Title = "Something else";
+            wnd.Show();
+        }
     }
 }
